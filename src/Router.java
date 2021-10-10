4a30aa6168a3dd222edba27b6dac76729abf5c38
SERGIOGIT777 <89037351+SERGIOGIT777@users.noreply.github.com>
2021-10-10 10:06:55 +0300

    Thread_otchet

--- src/Router.java
+++ src/Router.java
@@ -3,7 +3,15 @@ import controller.ApplicationController;
 import controller.IController;
 import controller.PhonebookController;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Router {
     private String[] arguments;
@@ -19,6 +27,23 @@ public class Router {
             controller = new ApplicationController();
         } else if (action.startsWith("phonebook/")) {
             controller = new PhonebookController();
+            Thread thread = new Thread(() -> {
+                try {
+                    List<File> files = Files.walk(Paths.get("/home/sergio/Стол/Phonebook/out/production/Phonebook/"))
+                            .filter(Files::isRegularFile)
+                            .filter(path -> path.toString().endsWith(".class"))
+                            .map(Path::toFile)
+                            .collect(Collectors.toList());
+                    var write = new FileWriter("./otchet.json");
+                    write.write(String.valueOf(files));
+                    write.write("\n");
+                    write.write("Количество объектов = " + files.size());
+                    write.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            thread.start();
         } else {
             throw new IllegalArgumentException("Wrong action: " + action);
         }
